0b7efcd6da3a6cd2fe07d635eacb9debbc76cfbf
Piotr Sikora <piotrsikoraaa@gmail.com>
2020-09-23 16:50:00 +0200

    MongoDB Atlas authentication

--- db/db.go
+++ db/db.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -16,13 +18,22 @@ type Repository struct {
 
 // Init initializes connection to the database
 func Init() *mongo.Client {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.TODO(), clientOptions)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	password, ok := os.LookupEnv("MONGO_PASSWORD")
+	if !ok {
+		log.Fatal("Environmental variable for password not set")
+	}
+	uri := fmt.Sprintf("mongodb+srv://user:%s@cluster0.hnspb.gcp.mongodb.net/moviesiec?retryWrites=true&w=majority", password)
+
+	clientOptions := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = client.Ping(context.TODO(), nil)
+	err = client.Ping(ctx, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
