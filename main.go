22a390b374bb78ef009c0f5214ef486f563f2d54
Piotr Sikora <piotrsikoraaa@gmail.com>
2020-09-24 14:53:02 +0200

    Removed service

--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"context"
+	"log"
+	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/piotsik/moviesiec/db"
+	"github.com/piotsik/moviesiec/listing"
 )
 
 func main() {
 	client := db.Init()
 	defer client.Disconnect(context.TODO())
+
+	router := httprouter.New()
+	router.GET("/movies", listing.MakeGetMoviesEndpoint(client))
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
